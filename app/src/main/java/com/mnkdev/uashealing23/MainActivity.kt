1bca83e901d560f239e8a20d2b1f93a5727ba4f5
Gabriel Narendra <gabrielnarendra1@gmail.com>
2025-06-25 00:04:41 +0700

    bugged on AGP(?)

--- app/src/main/java/com/mnkdev/uashealing23/MainActivity.kt
+++ app/src/main/java/com/mnkdev/uashealing23/MainActivity.kt
@@ -1,23 +1,50 @@
 package com.mnkdev.uashealing23
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.navigation.NavigationView
 import com.mnkdev.uashealing23.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     val fragments: ArrayList<Fragment> = ArrayList()
+    private lateinit var drawerLayout: DrawerLayout //init drawer
+    private lateinit var navView: NavigationView //init view
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //bagian drawer
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_change_password -> {
+                    Toast.makeText(this, "Change Password clicked", Toast.LENGTH_SHORT).show()
+                    // TODO: Ganti ke fragment atau activity change password
+                }
+                R.id.nav_logout -> {
+                    Toast.makeText(this, "Logging out...", Toast.LENGTH_SHORT).show()
+                    // TODO: Tambahkan logic logout, misal hapus session dan navigate ke login
+                }
+            }
+            drawerLayout.closeDrawers()
+            true
+        }
+
         fragments.add(ExploreFragment())
         fragments.add(FavoriteFragment())
         fragments.add(ProfileFragment())
